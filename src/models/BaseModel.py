e0e5e792b2cf935cb3c5bc6c8336a1fe0b7ead46
jorsae <jorgen.sater@gmail.com>
2022-12-26 18:23:51 +1100

    Set check_same_thread to False

--- src/models/BaseModel.py
+++ src/models/BaseModel.py
@@ -1,7 +1,7 @@
 import peewee as pw
 import constants
 
-database = pw.SqliteDatabase(None)
+database = pw.SqliteDatabase(None, check_same_thread=False)
 
 class BaseModel(pw.Model):
     class Meta:
