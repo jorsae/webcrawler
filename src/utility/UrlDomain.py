9fd9ad8f5e9cd83641896d124b55e5a5bdffd6d0
jorsae <jorgen.sater@gmail.com>
2023-01-06 23:33:55 +1100

    Added check when UrlDomain is None

--- src/utility/UrlDomain.py
+++ src/utility/UrlDomain.py
@@ -30,7 +30,11 @@ class UrlDomain:
             return True
     
     def __lt__(self, other):
-        if self.domain[0].id < other.domain[0].id:
+        try:
+            if self.domain[0].id < other.domain[0].id:
+                return True
+        except Exception as e:
+            logging.warning(self)
             return True
     
     def __cmp__(self, other):
