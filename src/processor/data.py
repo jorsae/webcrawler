dde1ef880813c27d879fcc4c4589836c7c944baa
jorsae <jorgen.sater@gmail.com>
2023-01-21 17:24:23 +1100

    Added exception handling to processor

--- src/processor/data.py
+++ src/processor/data.py
@@ -1,15 +1,20 @@
 from bs4 import BeautifulSoup
 from bs4.element import Comment
+import logging
 
 def get_visible_data(content):
-    soup = BeautifulSoup(content, "html.parser")
-    for script in soup(["script", "style"]):
-        script.extract() # rip it out
-    
-    text = soup.get_text()
-    lines = (line.strip() for line in text.splitlines())
-    chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
-    return '\n'.join(chunk for chunk in chunks if chunk)
+    try:
+        soup = BeautifulSoup(content, "html.parser")
+        for script in soup(["script", "style"]):
+            script.extract() # rip it out
+        
+        text = soup.get_text()
+        lines = (line.strip() for line in text.splitlines())
+        chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+        return '\n'.join(chunk for chunk in chunks if chunk)
+    except Exception as e:
+        logging.error(e)
+        return ''
 
 def tag_visible(element):
     if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
--- src/processor/url.py
+++ src/processor/url.py
@@ -7,15 +7,19 @@ from utility import UrlDomain
 import constants
 
 def get_urls(url, content):
-    logging.debug(f'get_urls Processing: {url}')
-    soup = BeautifulSoup(content, "html.parser")
-    urls = []
-    for link in soup.findAll('a', href=True):
-        link = urljoin(url, link['href'])
-        if re.match(constants.REGEX_URL, link) is not None:
-            urls.append(UrlDomain(link))
-    
-    return urls
+    try:
+        logging.debug(f'get_urls Processing: {url}')
+        soup = BeautifulSoup(content, "html.parser")
+        urls = []
+        for link in soup.findAll('a', href=True):
+            link = urljoin(url, link['href'])
+            if re.match(constants.REGEX_URL, link) is not None:
+                urls.append(UrlDomain(link))
+        
+        return urls
+    except Exception as e:
+        logging.error(e)
+        return []
 
 def get_emails(url, content):
     # TODO: Remove duplicate
