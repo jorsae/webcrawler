ef6e1ae3fab5ac2c0e7e94a8a7ad499c7dace409
jorsae <jorgen.sater@gmail.com>
2023-06-04 11:09:25 +1000

    CrawlHistoryModel.url is no longer a unique field.

--- src/models/CrawlHistoryModel.py
+++ src/models/CrawlHistoryModel.py
@@ -5,7 +5,7 @@ from models import BaseModel, DomainModel, RequestStatusModel
 
 class CrawlHistoryModel(BaseModel):
     id = pw.AutoField(primary_key=True)
-    url = pw.TextField(unique=True)
+    url = pw.TextField()
     timestamp = pw.DateTimeField()
     http_status_code = pw.SmallIntegerField()
     request_status = pw.ForeignKeyField(RequestStatusModel, to_field="id")
